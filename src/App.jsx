5c7460db13165c50d22370dea544cb139efe0a04
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-09-23 12:35:59 +0530

    Added webcam, trying facemesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: {
+        width: 640,
+        height: 480,
+      },
+      scale: 0.8,
+    });
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    console.log(webcamRef.current);
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      typeof webcamRef.current !== null &&
+      typeof webcamRef.current.video.readyState === 4
+    ) {
+      // Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set Webcam dimensions
+      webcamRef.current.video.videoWidth = videoWidth;
+      webcamRef.current.video.videoHeight = videoHeight;
+
+      // Set canvas dimensions
+      canvasRef.current.Width = videoWidth;
+      canvasRef.current.Height = videoHeight;
+
+      // Detections
+      const face = await net.estimateFaces(video);
+      console.log(face);
+    }
+  };
+
+  runFacemesh();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3 style={{ textAlign: "center", marginTop: 20 }}>Webcam</h3>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          height: 480,
+          zIndex: 10,
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          height: 480,
+          zIndex: 11,
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
