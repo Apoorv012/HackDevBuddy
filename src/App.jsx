a32c136b647141c8bdf64abe0a51ba43c662ff85
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-09-23 19:59:48 +0530

    Error with webgazer

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,10 @@
 import { useState, useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
+import webgazer from "webgazer";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-
-  const runFacemesh = async () => {
-    const net = await facemesh.load({
-      inputResolution: {
-        width: 640,
-        height: 480,
-      },
-      scale: 0.8,
-    });
-
-    setInterval(() => {
-      detect(net);
-    }, 1000);
-  };
-
-  const detect = async (net) => {
-    const currWebcamRef = webcamRef.current;
-    if (
-      typeof currWebcamRef !== "undefined" &&
-      typeof currWebcamRef !== null &&
-      currWebcamRef?.video.readyState === 4
-    ) {
-      // Video Properties
-      const video = await currWebcamRef.video;
-      const videoWidth = await currWebcamRef.video.videoWidth;
-      const videoHeight = await currWebcamRef.video.videoHeight;
-
-      // Set canvas dimensions
-      canvasRef.current.Width = videoWidth;
-      canvasRef.current.Height = videoHeight;
-
-      // Detections
-      const face = await net.estimateFaces(video);
-      console.log(face);
-    }
-  };
-
-  runFacemesh();
 
   return (
     <>
