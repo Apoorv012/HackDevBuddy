24d0647797c4712b85f2cd6aafdeefdc3a004d58
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-09-23 22:52:27 +0530

    Everything working great, starting with eye-tracking

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  console.log(webgazer);
+  webgazer
+    .setGazeListener(function (data, elapsedTime) {
+      if (data == null) {
+        return;
+      }
+      var xprediction = data.x; //these x coordinates are relative to the viewport
+      var yprediction = data.y; //these y coordinates are relative to the viewport
+      console.log(elapsedTime); //elapsed time is based on time since begin was called
+    })
+    .begin();
 
   return (
     <>
